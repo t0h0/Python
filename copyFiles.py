7434c5da5b7cc6448dd8e191e3d4abdbc277d8d8
Alexey <akat0h0@gmail.com>
2013-04-15 15:23:17 +0400

    Update copyFiles.py
    copyAndSort by date of create file. All extension. 
    For example: 
    ----Select directory: C:\test\ 
    ---Select new dir: C:\new\ 
    ---Select extension: .jpeg

--- copyFiles.py
+++ copyFiles.py
@@ -1,50 +1,38 @@
 # -*- coding: UTF-8 -*-
 #----------------sort files when he was created-------------------
- 
+
 import os
-import glob
 import time
 import shutil
- 
+
 d = raw_input('Select directory: ') # Set dir with only files
 newd = raw_input('Select new dir: ') # Set new dir
-if not os.path.exists(newd): # if new dir don't exist -> create
-    os.makedirs(newd)
-d = d + "*.jpg" # set type of file "*txt"
-names = glob.glob(d)
-print('----------------------PROCESSING--------------------\n ')
-for name in names:
- 
+ext = raw_input('Select extension: ')
+
 #-------------------work with file------------------
- 
-    dataCreate = os.stat(name).st_ctime # function os.stat().ctime return datecreate of file in seconds
-	dataCreate = time.localtime(dataCreate) # transform seconds in good date
-	year = str(dataCreate.tm_year)# from good date select year
-	month = str(dataCreate.tm_mon)
-	day = str(dataCreate.tm_mday)
-	nameNewDir = year+'-'+month+'-'+day
-	(dirname, filename)= os.path.split(name) # split on directory and gile name
-	print('>>> '+name+' create on '+nameNewDir)
- 
+
+print ('\n-----------------------[PROCESSING]----------------\n')
+for root, dirs, files in os.walk(d):
+	for name in files:
+		fullname = os.path.join(root, name)
+		(dirname, filename) = os.path.split(fullname)
+		(shortname, extension) = os.path.splitext(filename)
+		if extension == ext: #set extension
+			dataCreate = os.stat(fullname).st_ctime # function os.stat().ctime return datecreate of file in seconds
+			dataCreate = time.localtime(dataCreate) # transform seconds in good date
+			year = str(dataCreate.tm_year)# from good date select year
+			month = str(dataCreate.tm_mon)
+			day = str(dataCreate.tm_mday)
+			nameNewDir = year+'-'+month+'-'+day
+			print('>>> '+fullname+' created on '+nameNewDir)
+
 #----------------work with directory-------------
- 
-	alldf = os.listdir(newd) # list created directory
-	if os.listdir(newd): 
-		for oned in alldf:
-			dtc = newd+oned 
-			if os.path.isdir(dtc): # if is dir
-				if oned == nameNewDir: # if dir exist
-					shutil.copy2(name,dtc) # cope file
-					print('    '+filename+' copeyd in '+dtc+'\n')
-					break
-				else:
-					ndtc = newd+nameNewDir
-					os.makedirs(ndtc) 
-					shutil.copy2(name,ndtc) # copw file
-					print('    '+filename+' copeyd in '+ndtc+'\n')
-					break
-	else: # if list don't exist 
-		ndtc = newd+nameNewDir
-		os.makedirs(ndtc)
-		shutil.copy2(name,ndtc)
-		print('    '+filename+' copeyd in '+ndtc+'\n')
+
+			ndc = newd+nameNewDir
+			if os.path.exists(ndc):
+				shutil.copy2(fullname,ndc) # copy file
+				print('    '+name+' copy in '+ndc+'\n')
+			else:
+				os.makedirs(ndc)
+				shutil.copy2(fullname,ndc) # copy file
+				print('    '+name+' copy in '+ndc+'\n')
